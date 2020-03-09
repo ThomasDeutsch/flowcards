e25aec2abca08092ba28d7d04b38e105736a4609
thomasdeutsch <deutsch.thomas@gmail.com>
2020-03-09 19:24:31 +0100

    disable any lint

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 import { ScaffoldingFunction, createUpdateLoop, DispatchedActions, ScenarioStates } from './update-loop';
 import { Logger } from "./logger";
 
