d7931d6714ba46be92cf87dfb19086c18e3d14e5
thomasdeutsch <deutsch.thomas@gmail.com>
2020-04-01 14:26:44 +0200

    export stateref

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,3 @@
 export { useOverrides } from './overrides-hook';
 export { useScenarios } from './scenarios-hook';
-export { wait, block, intercept, request, ThreadContext, DispatchedAction } from '@flowcards/core'
+export { wait, block, intercept, request, ThreadContext, DispatchedAction, StateRef } from '@flowcards/core';
