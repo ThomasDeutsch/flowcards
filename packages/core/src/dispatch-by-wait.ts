b943d2581aa5b9ba93a998fc73293510d55fa989
thomasdeutsch <deutsch.thomas@gmail.com>
2020-04-05 21:11:05 +0200

    a guard can return undefined

--- packages/core/src/dispatch-by-wait.ts
+++ packages/core/src/dispatch-by-wait.ts
@@ -4,7 +4,7 @@ import { Bid, BidArrayDictionary, GuardFunction } from './bid';
 import { ActionType } from './action';
 import { DispatchFunction } from './update-loop';
 
-type TriggerDispatch = Function | null;
+type TriggerDispatch = Function | undefined;
 export type GuardedDispatch = (valueToDispatch: any) => TriggerDispatch;
 export type DispatchByWait = Record<string, GuardedDispatch>;
 interface EventCache {
@@ -46,7 +46,7 @@ export function dispatchByWait(dispatch: DispatchFunction, dbw: DispatchByWait, 
                 if(combinedGuardByWait[eventName](payload) && cache.dispatch) {
                     return cache.dispatch;
                 } else {
-                    return null;
+                    return undefined;
                 }
             }
         }
