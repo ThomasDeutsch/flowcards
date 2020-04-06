191e99b16c822084e9557e9d14581139ebf0fd16
Thomas Deutsch <deutsch.thomas@gmail.com>
2020-04-06 07:27:49 +0200

    export DispatchByWait

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,3 @@
 export { useOverrides } from './overrides-hook';
 export { useScenarios } from './scenarios-hook';
-export { wait, block, intercept, request, BTContext, DispatchedAction, StateRef, GuardedDispatch } from '@flowcards/core';
+export { wait, block, intercept, request, BTContext, DispatchedAction, StateRef, GuardedDispatch, DispatchByWait } from '@flowcards/core';
