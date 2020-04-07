19c4cc31dcf292c5739ef29a31ea2b7773f3c27d
Thomas Deutsch <deutsch.thomas@gmail.com>
2020-04-07 08:37:53 +0200

    added a test for canceled promises

--- packages/core/test/scenario.test.ts
+++ packages/core/test/scenario.test.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import * as bp from "../src/bid";
-import { scenarios, BTContext } from '../src/index';
+import { scenarios, BTContext, ScaffoldingFunction, DispatchedAction, ScenariosContext, createUpdateLoop } from '../src/index';
 
 function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -39,4 +39,32 @@ test("there will be a dispatch-function every waiting event", () => {
         expect(scenario.dispatch.eventTwo).toBeDefined();
         expect(scenario.dispatch.eventThree).toBeDefined();
     });
+});
+
+
+function loggerScenarios(scaffoldingFn: ScaffoldingFunction, da: Set<string>): void {
+    const updateLoop = createUpdateLoop(scaffoldingFn, (a: DispatchedAction): void => {
+        if(a.payload) da.add(a.payload.eventName);
+        updateLoop(a);   
+    });
+    updateLoop(null);
+}
+
+test("if a request is rejected, it will fire no update", done => {
+    const dispatchedActions = new Set<string>();
+    
+    function* thread1() {
+        yield bp.request("cancel", delay(100));
+    }
+    function* thread2() {
+        const [type] = yield [bp.request('async-event', delay(500)), bp.wait('cancel')];
+        expect(type).toEqual('cancel');
+        yield bp.request("async-event-two", delay(1000));
+        expect(dispatchedActions.has('async-event')).toEqual(false);
+        done();
+    }
+    loggerScenarios((enable) => {
+        enable(thread1);
+        enable(thread2);
+    }, dispatchedActions);
 });
