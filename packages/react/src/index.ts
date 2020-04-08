6b6e596a02305b46a8157b4990260950f420d28e
thomasdeutsch <deutsch.thomas@gmail.com>
2020-04-08 16:31:50 +0200

    export Logger

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -7,3 +7,5 @@ export function useScenarios(stagingFunction: StagingFunction) : ScenariosContex
     const [state, setState] = useState((): ScenariosContext => scenarios(stagingFunction, (a: ScenariosContext): void => { setState(a) }, false));
     return state;
 }
+
+export { Logger } from './logger.component';
--- packages/react/src/logger.component.tsx
+++ packages/react/src/logger.component.tsx
@@ -1,5 +1,9 @@
-import { Logger } from "@flowcards/core";
+import { Log } from "@flowcards/core";
+import { ReactElement } from "react";
 
-export function Logger(logger: Logger) {
-    
+export function Logger(log?: Log): ReactElement {
+    if(!log) {
+        return <div>no log</div>;
+    }
+    else return <div>todo: log</div>;
 }
