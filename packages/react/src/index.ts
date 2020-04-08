5fdd4e12f73505f8475391f5881466c404c45bec
thomasdeutsch <deutsch.thomas@gmail.com>
2020-04-08 07:30:22 +0200

    removied react-hooks file and moved everything to index.ts

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,8 @@
-export { useScenarios } from './scenarios-hook';
+import { useState } from "react";
+import { ScaffoldingFunction, ScenariosContext, scenarios } from "@flowcards/core";
+
 export * from '@flowcards/core';
+export function useScenarios(scaffoldingFn: ScaffoldingFunction) : ScenariosContext {
+    const [state, setState] = useState((): ScenariosContext => scenarios(scaffoldingFn, (a: ScenariosContext): void => { setState(a) }, false));
+    return state;
+}
