701db71a8b0898b709eb229dc21a56763b9ccc17
thomasdeutsch <deutsch.thomas@gmail.com>
2020-04-08 07:11:08 +0200

    bugfix for setting initial state

--- packages/react/src/scenarios-hook.ts
+++ packages/react/src/scenarios-hook.ts
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import { ScaffoldingFunction, ScenariosContext, scenarios } from "@flowcards/core";
-import { DispatchedAction } from '../../core/build/update-loop';
-
-
 
 export function useScenarios(scaffoldingFn: ScaffoldingFunction) : ScenariosContext {
     const [state, setState] = useState((): ScenariosContext => scenarios(scaffoldingFn, (a: ScenariosContext): void => { setState(a) }, false));
     return state;
 }
-
 
 // Example: solution for using a useReducer instead of useState
 // export function useScenarios(scaffoldingFn: ScaffoldingFunction) : ScenariosContext | null {
