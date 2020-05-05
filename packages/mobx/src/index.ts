63ca64bad74ce3c9bea4dbdf083b22fd27becaf2
thomasdeutsch <deutsch.thomas@gmail.com>
2020-05-05 11:22:32 +0200

    make context public

--- packages/mobx/src/index.ts
+++ packages/mobx/src/index.ts
@@ -7,23 +7,23 @@ import { observable, decorate, computed } from "mobx";
 export * from '@flowcards/core';
 
 export class FlowcardsStore {
-  private _context: Fc.ScenariosContext;
+  public context: Fc.ScenariosContext;
   public dispatch: Fc.EventDispatch;
 
   constructor(stagingFunction: Fc.StagingFunction) {
-    [this._context, this.dispatch] = Fc.scenarios(stagingFunction),
+    [this.context, this.dispatch] = Fc.scenarios(stagingFunction),
       (updatedContext: Fc.ScenariosContext) => {
-        this._context = updatedContext;
+        this.context = updatedContext;
     }
   }
   latest = computedFn(function(this: FlowcardsStore, event: FCEvent | string) {
-    return this._context.latest(event);
+    return this.context.latest(event);
   });
   isPending = computedFn(function(this: FlowcardsStore, event: FCEvent | string) {
-    return this._context.isPending(event);
+    return this.context.isPending(event);
   });
   get bTState() {
-    return this._context.bTState;
+    return this.context.bTState;
   }
 }
 
