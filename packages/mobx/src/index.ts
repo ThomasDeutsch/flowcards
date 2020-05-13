58870ac0e52460b7f9677c904a4ada8f446c664c
thomasdeutsch <deutsch.thomas@gmail.com>
2020-05-13 12:32:19 +0200

    mobx: make dispatch a computedFn

--- packages/mobx/src/index.ts
+++ packages/mobx/src/index.ts
@@ -2,18 +2,22 @@
 import * as Fc from "@flowcards/core";
 import { computedFn } from "mobx-utils";
 import { observable, decorate, computed } from "mobx";
+import { FCEvent } from '../../core/build/event';
 
 export * from '@flowcards/core';
 
 export class FlowcardsStore {
   public context: Fc.ScenariosContext;
-  public dispatch: Fc.EventDispatch;
+  private _dispatch: Fc.EventDispatch;
 
   constructor(stagingFunction: Fc.StagingFunction) {
-    [this.context, this.dispatch] = Fc.scenarios(stagingFunction, (updatedContext: Fc.ScenariosContext) => {
+    [this.context, this._dispatch] = Fc.scenarios(stagingFunction, (updatedContext: Fc.ScenariosContext) => {
       this.context = updatedContext;
     });
   }
+  dispatch = computedFn(function(this: FlowcardsStore, eventName: string, eventKey?: string | number, payload?: any) {
+    return this._dispatch({name: eventName, key: eventKey}, payload);
+  })
   latest = computedFn(function(this: FlowcardsStore, eventName: string, eventKey?: string | number) {
     return this.context.latest(eventName, eventKey);
   });
