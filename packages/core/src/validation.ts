59659aa3e47cc960f65b6e586b8239872aa72693
Thomas Deutsch <thomas.deutsch@adesso.de>
2021-11-08 07:59:00 +0100

    added failed array to falication return

--- packages/core/src/validation.ts
+++ packages/core/src/validation.ts
@@ -1,14 +1,20 @@
 import { PlacedBid } from './bid';
 
-export type PayloadValidationResult<V> = boolean | V[];
+export type PayloadValidationResult<V> = boolean | V[] | { failed?: V[], passed?: V[] };
 export type PayloadValidationCB<P, V> = (value: P) => PayloadValidationResult<V>;
 
 
 export function isValidReturn(result: PayloadValidationResult<any>): boolean {
-    return result === true || (typeof result === 'object' && result.length > 0);
+    if(typeof result === 'boolean') {
+        return result;
+    }
+    if(Array.isArray(result)) {
+        return result.length === 0;
+    }
+    return !result.failed?.length;
 }
 
-export type ValidationResults<V> = { isValid: boolean, failed: V[] };
+export type ValidationResults<V> = { isValid: boolean, failed: V[], passed: V[] };
 
 export function getAllPayloadValidationCallbacks<P, V>(bid: PlacedBid<P>, validateBids?: PlacedBid[]): PayloadValidationCB<P, V>[] {
     const validationCallbacks = [];
@@ -27,16 +33,30 @@ export function isValidPayload<P>(validationCallbacks: PayloadValidationCB<P, an
     return validationCallbacks.every(cb => isValidReturn(cb(value)));
 }
 
-export function validateAll<P>(validationCallbacks: PayloadValidationCB<P, unknown>[], value: P): ValidationResults<any> {
-    const failed: any[] = [];
-    validationCallbacks.forEach(validationCB => {
+export function validateAll<P, V>(validationCallbacks: PayloadValidationCB<P, V>[], value: P): ValidationResults<any> {
+    let booleanFailed = false;
+    let failed: V[] = [];
+    let passed: V[] = [];
+    validationCallbacks.forEach((validationCB) => {
         const validationResult = validationCB(value);
-        if(!isValidReturn(validationResult)) {
-            failed.push(validationResult)
+        if(typeof validationResult === 'boolean') {
+            booleanFailed = true;
+            return;
+        }
+        if(Array.isArray(validationResult)) {
+            failed = failed.concat(validationResult);
+            return;
+        }
+        if(validationResult.failed?.length){
+            failed = failed.concat(validationResult.failed);
+        }
+        if(validationResult.passed?.length) {
+            passed = passed.concat(validationResult.passed);
         }
     });
     return {
-        isValid: failed.length === 0,
-        failed: failed
+        isValid: booleanFailed || failed.length === 0,
+        failed: failed,
+        passed: passed
     };
 }
