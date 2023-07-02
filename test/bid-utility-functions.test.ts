072a7b244fdd5a79eb674546f8105e0969e21631
Thomas Deutsch <thomas.deutsch@adesso.de>
2023-07-02 21:24:34 +0200

    fix bug in getEvents function

--- test/bid-utility-functions.test.ts
+++ test/bid-utility-functions.test.ts
@@ -1,5 +1,5 @@
 import { Flow } from "../src/flow";
-import { Event } from "../src/event";
+import { Event, EventByKey, NestedEventObject } from "../src/event";
 import { askFor, request, waitFor } from "../src/bid";
 import { testSchedulerFactory } from "./utils";
 import { extendAll, getAllValues, getFirstValue, getValue } from "../src";
@@ -33,23 +33,46 @@ describe("different flow utility functions", () => {
     });
 
     test("extend all values that are asked for", (done) => {
-        const eventA = new Event<number>('eventA');
-        const eventB = new Event<number>('eventB');
+        const event = {
+            root: {
+              dialog: new Event<number | null>('rootDialog'),
+              allMatArtCodesResult: new Event<number[] | undefined>('root.matArtCodeResult'),
+              serviceStatus: new Event<number>('root.serviceStatus'),
+            },
+            riskCharacteristic: {
+              search: new Event<number>('riskCharacteristic.search'),
+              searchResult: new Event<number | undefined>('riskCharacteristic.searchResult'),
+              selectEditMode: new Event<number | undefined>('riskCharacteristic.selectEditMode'),
+              selectCreateMode: new Event('riskCharacteristic.selectCreateMode'),
+              selectAssignMode: new Event<number | undefined>('riskCharacteristic.selectAssignMode'),
+              result: new Event<number | undefined>('riskCharacteristic.result'),
+              save: new Event<number>('riskCharacteristic.save'),
+              abort: new Event<boolean>('riskCharacteristic.abort'),
+              parent: new Event<number | string>('riskCharacteristic.parent'),
+              parentResults: new Event<number[] | undefined>('riskCharacteristic.parentResults'),
+              parentSearch: new EventByKey<number | undefined>('riskCharacteristic.parentSearch'),
+              allCharacteristicDataTypes: new Event<number[]>('riskCharacteristic.allCharacteristicDataTypes'),
+              leaveEditModeWithoutSave: new Event<boolean>('riskCharacteristic.leaveEditModeWithoutSave'),
+              editedCharacteristicHasChanges: new Event<boolean>('riskCharacteristic.editedCharacteristicHasChanges'),
+            }
+          } as const satisfies NestedEventObject;
+
         testSchedulerFactory( function*(this: Flow) {
             const extendingFlow = this.flow('extendingFlow', function*() {
-                const [extendedEvent] = yield* extendAll([eventA, eventB], (event) => event.isAskedFor);
-                expect(extendedEvent).toBe(eventA);
+                const [extendedEvent] = yield* extendAll([event], (event) => event.isAskedFor);
+                expect(extendedEvent).toBe(event.riskCharacteristic.search);
                 done();
             }, []);
             this.flow('requestingFlow', function*() {
-                yield request(eventB, 100);
-                yield request(eventA, 200)
+                yield request(event.riskCharacteristic.search, 100);
+                yield request(event.root.dialog, 200)
             }, []);
             this.flow('askingFlow', function*() {
-                yield askFor(eventA);
+                yield askFor(event.riskCharacteristic.search);
             }, []);
             yield undefined;
         });
     });
 })
+
 
